ae6caf5bc6d13c57a43306438f23cfff21861ce9
amwcandy <alex.m.wortmann@gmail.com>
2022-02-08 22:35:23 -0600

    added neopixel library

---
+++ Lighting Board/Code/LightingRev1.cpp
@@ -0,0 +1,4 @@
+#include "Adafruit_NeoPixel.h"
+#include "LightingBoard_Software.h"
+#include "RoveComm.h"
+
---
+++ Lighting Board/Code/LightingRev1.h
@@ -0,0 +1,8 @@
+#ifndef _LightingBoard
+#define _LightingBoard
+
+#include <Energia.h> 
+#include "RoveComm.h"
+#include "Adafruit_NeoPixel.h"
+
+#endif
