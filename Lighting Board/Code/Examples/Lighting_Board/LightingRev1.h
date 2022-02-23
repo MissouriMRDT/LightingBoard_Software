3a88b99f43fcbd89afb3d8e9e923b0f393b77252
amwcandy <alex.m.wortmann@gmail.com>
2022-02-22 23:23:28 -0600

    Changed stuff
    Changed Neopixel library
    only 1 neopixel object will work after .begin();
    if >=2 .begin(); exist then no neopixels work

--- Lighting Board/Code/Examples/Lighting_Board/LightingRev1.h
+++ Lighting Board/Code/Examples/Lighting_Board/LightingRev1.h
@@ -14,10 +14,10 @@ uint32_t last_update_time;
 //declare the Ethernet Server in the top level sketch with the requisite port ID any time you want to use RoveComm
 EthernetServer TCPServer(RC_ROVECOMM_MULTIMEDIABOARD_PORT);
 
-#define BRIGTNESS       50
-#define AUTONOMY_COUNT  255
-#define UNDERGLOW_COUNT 255
-#define SPEAKER_COUNT   255
+#define BRIGTNESS       20
+#define AUTONOMY_COUNT  256
+#define UNDERGLOW_COUNT 256
+#define SPEAKER_COUNT   256
 #define INTERIOR_COUNT  0
 
 #define HEADLIGHT_TOGGLE  PE_3  
@@ -26,9 +26,9 @@ EthernetServer TCPServer(RC_ROVECOMM_MULTIMEDIABOARD_PORT);
 #define SPEAKER_PANEL		2 //PD_1
 #define INTERIOR_STRIP      1 //PE_4
 
-Adafruit_NeoPixel autonomy(AUTONOMY_COUNT,AUTONOMY_PANEL,NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel underglow(UNDERGLOW_COUNT,UNDERGLOW_PANEL,NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel speaker(SPEAKER_COUNT,SPEAKER_PANEL,NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel interior(INTERIOR_COUNT,INTERIOR_STRIP,NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel autonomy = Adafruit_NeoPixel(AUTONOMY_COUNT,AUTONOMY_PANEL,NEO_GRB);
+Adafruit_NeoPixel underglow = Adafruit_NeoPixel(UNDERGLOW_COUNT,UNDERGLOW_PANEL,NEO_GRB);
+Adafruit_NeoPixel speaker = Adafruit_NeoPixel(SPEAKER_COUNT,SPEAKER_PANEL,NEO_GRB);
+Adafruit_NeoPixel interior = Adafruit_NeoPixel(INTERIOR_COUNT,INTERIOR_STRIP,NEO_GRB);
 
 #endif
--- Lighting Board/Code/Examples/Lighting_Board/Lighting_Board.ino
+++ Lighting Board/Code/Examples/Lighting_Board/Lighting_Board.ino
@@ -14,27 +14,19 @@ void setup()
   //update timekeeping
   last_update_time = millis();
 
-  pinMode(AUTONOMY_PANEL,OUTPUT);
-
+  
   autonomy.begin();
-  autonomy.setBrightness(255);
-  autonomy.clear();
-  autonomy.show();
-  underglow.begin();
-  underglow.setBrightness(BRIGTNESS);
-  underglow.show();
-  speaker.begin();
-  speaker.setBrightness(BRIGTNESS);
-  speaker.show();
-  interior.begin();
-  interior.setBrightness(BRIGTNESS);
-  interior.show();
+  //underglow.begin();
+
+  //speaker.begin();
+  //interior.begin();
+  autonomy.setBrightness(5);
+  //underglow.setBrightness(20);
 }
 
 void loop()
 {
   packet = RoveComm.read();
-  uint32_t red = 0x00FF0000;
   if (packet.data_id != 0)
   {
     switch (packet.data_id)
@@ -46,66 +38,59 @@ void loop()
         case RC_MULTIMEDIABOARD_LEDPATTERNS_DATA_ID:
           break;
         case RC_MULTIMEDIABOARD_STATEDISPLAY_DATA_ID:
-          //uint8_t* displayState = (uint8_t*)packet.data;
-          //Serial.println(displayState[0]);
+          uint8_t* displayState = (uint8_t*)packet.data;
           switch (displayState[0])
             {
               case TELEOP:
-                //Serial.println("Ya mum");
-                //autonomy.setPixelColor(1, 0, 255, 255);
-                //autonomy.show();
-                //autonomy.fill(red,0,AUTONOMY_COUNT);
-                //autonomy.show();
-                //Serial.println("Ya mum2");
-                //Serial.println(autonomy.canShow());
                 for( uint16_t i=0; i<AUTONOMY_COUNT; i++)
                 {
-                  autonomy.setPixelColor(i, red);
+                  autonomy.setPixelColor(i, autonomy.Color(0,0,255));
                   autonomy.show();
-                  speaker.setPixelColor(i, speaker.Color(0,0,255));
-                  speaker.show();
+                  //speaker.setPixelColor(i, speaker.Color(0,0,255));
+                  //speaker.show();
+                }
+                /*for( uint16_t i=0; i<UNDERGLOW_COUNT; i++)
+                {
                   underglow.setPixelColor(i, underglow.Color(0,0,255));
                   underglow.show();
                 }
-                //Serial.println("Ya mum3");
-                for( uint16_t i=0; i<interior.numPixels(); i++)
+                /*for( uint16_t i=0; i<interior.numPixels(); i++)
                 {
                   interior.setPixelColor(i, interior.Color(0,0,255));
                   interior.show();
-                }
-                //Serial.println("Ya mum4");
+                }*/
                 break;
               case AUTONOMY:
-                for( uint16_t i=0; i<autonomy.numPixels(); i++)
+                for( uint16_t i=0; i<AUTONOMY_COUNT; i++)
                 {
                   autonomy.setPixelColor(i, autonomy.Color(255,0,0));
                   autonomy.show();
-                  speaker.setPixelColor(i, speaker.Color(255,0,0));
-                  speaker.show();
-                  underglow.setPixelColor(i, underglow.Color(255,0,0));
-                  underglow.show();
+                  //speaker.setPixelColor(i, speaker.Color(255,0,0));
+                  //speaker.show();
+                  //underglow.setPixelColor(i, underglow.Color(255,0,0));
+                  //underglow.show();
                 }
-                for( uint16_t i=0; i<interior.numPixels(); i++)
+                /*for( uint16_t i=0; i<interior.numPixels(); i++)
                 {
                   interior.setPixelColor(i, interior.Color(255,0,0));
                   interior.show();
-                }
+                }*/
                 break;
               case REACHED_GOAL:
-                for( uint16_t i=0; i<autonomy.numPixels(); i++)
+                for( uint16_t i=0; i<AUTONOMY_COUNT; i++)
                 {
                   autonomy.setPixelColor(i, autonomy.Color(0,255,0));
                   autonomy.show();
-                  speaker.setPixelColor(i, speaker.Color(0,255,0));
-                  speaker.show();
-                  underglow.setPixelColor(i, underglow.Color(0,255,0));
-                  underglow.show();
+                  //speaker.setPixelColor(i, speaker.Color(0,255,0));
+                  //speaker.show();
+                  //underglow.setPixelColor(i, underglow.Color(0,255,0));
+                  //underglow.show();
                 }
-                for( uint16_t i=0; i<interior.numPixels(); i++)
+                /*for( uint16_t i=0; i<interior.numPixels(); i++)
                 {
                   interior.setPixelColor(i, interior.Color(0,255,0));
                   interior.show();
-                }
+                }*/
                 break;
             }
           break;
