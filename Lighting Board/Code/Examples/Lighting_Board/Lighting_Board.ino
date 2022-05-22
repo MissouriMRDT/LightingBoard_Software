6249ed478dcf9db989c8247de95eae5dc78a9201
antrob25 <antrobles254@gmail.com>
2022-05-21 23:47:54 -0500

    fix formatting and review

--- Lighting Board/Code/Examples/Lighting_Board/Lighting_Board.ino
+++ Lighting Board/Code/Examples/Lighting_Board/Lighting_Board.ino
@@ -1,157 +1,156 @@
 #include "LightingRev1.h"
 
-void setup() 
+void setup()
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  //Set up rovecomm with the correct IP and the TCP server
-  RoveComm.begin(RC_MULTIMEDIABOARD_FOURTHOCTET, &TCPServer);
-  delay(100);
+    // Set up rovecomm with the correct IP and the TCP server
+    RoveComm.begin(RC_MULTIMEDIABOARD_FOURTHOCTET, &TCPServer);
+    delay(100);
 
-  Serial.println("Started Lighing Board");
-  
-  autonomy.begin();
-  autonomy.setBrightness(BRIGHTNESS);
-  pinMode(HEADLIGHT_TOGGLE, OUTPUT);
-  digitalWrite(HEADLIGHT_TOGGLE, LOW);
+    Serial.println("Started Lighing Board");
+
+    autonomy.begin();
+    autonomy.setBrightness(BRIGHTNESS);
+    pinMode(HEADLIGHT_TOGGLE, OUTPUT);
+    digitalWrite(HEADLIGHT_TOGGLE, LOW);
 }
 
 void loop()
 {
-  packet = RoveComm.read();
-  if (packet.data_id != 0)
-  {
-    switch (packet.data_id)
-      {
+    packet = RoveComm.read();
+    if (packet.data_id != 0)
+    {
+        switch (packet.data_id)
+        {
         case RC_MULTIMEDIABOARD_HEADLIGHTINTENSITY_DATA_ID:
         {
-          uint8_t* headlightEnable = (uint8_t*)packet.data;
-          if (headlightEnable[0] > 0)
-            digitalWrite(HEADLIGHT_TOGGLE, HIGH);
-          else
-            digitalWrite(HEADLIGHT_TOGGLE, LOW);
-          break;
+            uint8_t *headlightEnable = (uint8_t *)packet.data;
+            if (headlightEnable[0] > 0)
+                digitalWrite(HEADLIGHT_TOGGLE, HIGH);
+            else
+                digitalWrite(HEADLIGHT_TOGGLE, LOW);
+            break;
         }
         case RC_MULTIMEDIABOARD_LEDRGB_DATA_ID:
         {
-          uint8_t* LEDRGB = (uint8_t*)packet.data;
-          for( uint16_t i=0; i<AUTONOMY_COUNT; i++)
-          {
-            autonomy.setPixelColor(i, autonomy.Color(LEDRGB[0], LEDRGB[1], LEDRGB[2]));
-          }
-          autonomy.show();
-          break;
+            uint8_t *LEDRGB = (uint8_t *)packet.data;
+            for (uint16_t i = 0; i < AUTONOMY_COUNT; i++)
+            {
+                autonomy.setPixelColor(i, autonomy.Color(LEDRGB[0], LEDRGB[1], LEDRGB[2]));
+            }
+            autonomy.show();
+            break;
         }
         case RC_MULTIMEDIABOARD_LEDPATTERNS_DATA_ID:
         {
-          uint8_t* LEDPattern = (uint8_t*)packet.data;
-          switch (LEDPattern[0])
-          {
+            uint8_t *LEDPattern = (uint8_t *)packet.data;
+            switch (LEDPattern[0])
+            {
             case MRDT:
-              for(uint16_t i=131; i<(131+256); i++)
-              {
-                autonomy.setPixelColor(i, LOGO[i-131]);
-              }
-              autonomy.show();
-              break;
+                for (uint16_t i = 131; i < (131 + 256); i++)
+                {
+                    autonomy.setPixelColor(i, LOGO[i - 131]);
+                }
+                autonomy.show();
+                break;
             case BELGIUM:
-              for(uint16_t i=131; i<(131+256); i++)
-              {
-                autonomy.setPixelColor(i, BELGIUM_FLAG[i-131]);
-              }
-              autonomy.show();
-              break;
+                for (uint16_t i = 131; i < (131 + 256); i++)
+                {
+                    autonomy.setPixelColor(i, BELGIUM_FLAG[i - 131]);
+                }
+                autonomy.show();
+                break;
             case MERICA:
-              for(uint16_t i=131; i<(131+256); i++)
-              {
-                autonomy.setPixelColor(i, FLAG[i-131]);
-              }
-              autonomy.show();
-              break;
+                for (uint16_t i = 131; i < (131 + 256); i++)
+                {
+                    autonomy.setPixelColor(i, FLAG[i - 131]);
+                }
+                autonomy.show();
+                break;
             case DIRT:
-              for(uint16_t i=131; i<(131+256); i++)
-              {
-                autonomy.setPixelColor(i, DIRT_BLOCK[i-131]);
-              }
-              autonomy.show();
-              break;
+                for (uint16_t i = 131; i < (131 + 256); i++)
+                {
+                    autonomy.setPixelColor(i, DIRT_BLOCK[i - 131]);
+                }
+                autonomy.show();
+                break;
             case DOTA:
-              for(uint16_t i=131; i<(131+256); i++)
-              {
-                autonomy.setPixelColor(i, DOTES[i-131]);
-              }
-              autonomy.show();
-              break;
+                for (uint16_t i = 131; i < (131 + 256); i++)
+                {
+                    autonomy.setPixelColor(i, DOTES[i - 131]);
+                }
+                autonomy.show();
+                break;
             case MCD:
-              for(uint16_t i=131; i<(131+256); i++)
-              {
-                autonomy.setPixelColor(i, MCDONALD[i-131]);
-              }
-              autonomy.show();
-              break;
+                for (uint16_t i = 131; i < (131 + 256); i++)
+                {
+                    autonomy.setPixelColor(i, MCDONALD[i - 131]);
+                }
+                autonomy.show();
+                break;
             case WINDOWS:
-              for(uint16_t i=131; i<(131+256); i++)
-              {
-                autonomy.setPixelColor(i, MSW[i-131]);
-              }
-              autonomy.show();
-              break;
-          }
-          break;
+                for (uint16_t i = 131; i < (131 + 256); i++)
+                {
+                    autonomy.setPixelColor(i, MSW[i - 131]);
+                }
+                autonomy.show();
+                break;
+            }
+            break;
         }
         case RC_MULTIMEDIABOARD_STATEDISPLAY_DATA_ID:
         {
-          uint8_t* displayState = (uint8_t*)packet.data;
-          switch (displayState[0])
+            uint8_t *displayState = (uint8_t *)packet.data;
+            switch (displayState[0])
             {
-              case TELEOP:
-                for( uint16_t i=0; i<AUTONOMY_COUNT; i++)
+            case TELEOP:
+                for (uint16_t i = 0; i < AUTONOMY_COUNT; i++)
                 {
-                  autonomy.setPixelColor(i, autonomy.Color(0,0,255));
+                    autonomy.setPixelColor(i, autonomy.Color(0, 0, 255));
                 }
                 autonomy.show();
                 break;
-              case AUTONOMY:
-                for( uint16_t i=0; i<AUTONOMY_COUNT; i++)
+            case AUTONOMY:
+                for (uint16_t i = 0; i < AUTONOMY_COUNT; i++)
                 {
-                  autonomy.setPixelColor(i, autonomy.Color(255,0,0));
+                    autonomy.setPixelColor(i, autonomy.Color(255, 0, 0));
                 }
                 autonomy.show();
                 break;
-              case REACHED_GOAL:
-                for(int i = 0; i < 5; i++)
+            case REACHED_GOAL:
+                for (int i = 0; i < 5; i++)
                 {
-                  for( uint16_t i=0; i<AUTONOMY_COUNT; i++)
-                  {
-                    autonomy.setPixelColor(i, autonomy.Color(0,255,0));
-                  }
-                  autonomy.show();
-                  delay(500);
-                  for( uint16_t i=0; i<AUTONOMY_COUNT; i++)
-                  {
-                    autonomy.setPixelColor(i, autonomy.Color(0,0,0));
-                  }
-                  autonomy.show();
-                  delay(500);
+                    for (uint16_t i = 0; i < AUTONOMY_COUNT; i++)
+                    {
+                        autonomy.setPixelColor(i, autonomy.Color(0, 255, 0));
+                    }
+                    autonomy.show();
+                    delay(500);
+                    for (uint16_t i = 0; i < AUTONOMY_COUNT; i++)
+                    {
+                        autonomy.setPixelColor(i, autonomy.Color(0, 0, 0));
+                    }
+                    autonomy.show();
+                    delay(500);
                 }
                 break;
             }
-          break;
+            break;
         }
         case RC_MULTIMEDIABOARD_BRIGHTNESS_DATA_ID:
-          uint8_t* brightness = (uint8_t*)packet.data;
-          if(brightness[0]>=BRIGHTNESS)
-          {
-            autonomy.setBrightness(BRIGHTNESS);
-            autonomy.show();
-          }
-          else 
-          {
-           autonomy.setBrightness(brightness[0]);
-          autonomy.show();
-          }
-          break;
-      }
-  }
+            uint8_t *brightness = (uint8_t *)packet.data;
+            if (brightness[0] >= BRIGHTNESS)
+            {
+                autonomy.setBrightness(BRIGHTNESS);
+                autonomy.show();
+            }
+            else
+            {
+                autonomy.setBrightness(brightness[0]);
+                autonomy.show();
+            }
+            break;
+        }
+    }
 }
-
